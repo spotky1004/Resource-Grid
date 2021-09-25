529541c23f0aef098961554a95d4df2f1f30702d
Spotky1004 <spotky1004@gmail.com>
2021-09-26 01:34:49 +0900

    fix: Fix notation function

--- src/util/notation.js
+++ src/util/notation.js
@@ -1,9 +1,11 @@
-let notation1 = ["", "K", "M", "B", "T", "q", "Q", "s", "S", "o", "N"];
-let notation2 = ["", "D", "T", "q", "Q", "s", "S", "O", "N"];
+let notation1 = ["", "K", "M", "B", "T", "q", "Q", "s", "S", "O", "N"];
+let notation2 = ["", "U", "D", "T", "q", "Q", "s", "S", "O", "N"];
+let notation3 = ["", "D", "V", "T", "q", "Q", "s", "S", "O", "N"];
 export default function notation(x) {
   let OOM = Math.floor(Math.log10(Math.max(1, Math.abs(x))));
-  let suffixPart = notation1[Math.floor(OOM/3)%10] + notation2[Math.floor(OOM/30)];
-  let numberPart = (x/10**(Math.floor(OOM/3)*3)).toFixed(3-suffixPart.length-OOM%3);
+  let suffixLevel = Math.floor(OOM/3);
+  let suffixPart = (suffixLevel < 10 ? notation1[suffixLevel] : notation2[suffixLevel%10]) + notation3[Math.floor(suffixLevel/10)];
+  let numberPart = (x/10**(suffixLevel*3)).toFixed(Math.max(0, 3-suffixPart.length-OOM%3));
   if (x < 1000 && Number.isInteger(x)) numberPart = x;
   return numberPart + suffixPart;
 }
