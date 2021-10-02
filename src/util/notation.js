c3169243c0ae4082f3c61b3fd41e94f53c770e42
Spotky1004 <spotky1004@gmail.com>
2021-10-02 16:56:13 +0900

    fix: Fix notation

--- src/util/notation.js
+++ src/util/notation.js
@@ -4,7 +4,7 @@ let notation3 = ["", "D", "V", "T", "q", "Q", "s", "S", "O", "N"];
 export default function notation(x) {
   let OOM = Math.floor(Math.log10(Math.max(1, Math.abs(x))));
   let suffixLevel = Math.floor(OOM/3);
-  let suffixPart = (suffixLevel < 10 ? notation1[suffixLevel] : notation2[suffixLevel%10]) + notation3[Math.floor(suffixLevel/10)];
+  let suffixPart = (suffixLevel <= 10 ? notation1[suffixLevel] : notation2[suffixLevel%10]) + notation3[Math.floor(suffixLevel/10)];
   let numberPart = (x/10**(suffixLevel*3)).toFixed(Math.max(0, 3-suffixPart.length-OOM%3));
   if (x < 1000 && Number.isInteger(x)) numberPart = x;
   return numberPart + suffixPart;
