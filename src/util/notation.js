d0412875cd552bdd4346c1f0500dd15b5bae747f
Spotky1004 <spotky1004@gmail.com>
2021-10-02 23:22:04 +0900

    chore: Fix & Reinforce notation function

--- src/util/notation.js
+++ src/util/notation.js
@@ -1,11 +1,24 @@
 let notation1 = ["", "K", "M", "B", "T", "q", "Q", "s", "S", "O", "N"];
 let notation2 = ["", "U", "D", "T", "q", "Q", "s", "S", "O", "N"];
 let notation3 = ["", "D", "V", "T", "q", "Q", "s", "S", "O", "N"];
-export default function notation(x) {
+export default function notation(x, precision=3, focusDecimal=false) {
+  let numberPartLength = precision;
+  let extra = "";
+
+  let sign = Math.sign(x);
+  if (sign === -1) numberPartLength -= 1;
+  x = Math.abs(x);
+  if (x !== 0 && x < 10**(-precision)) {
+    x = x**-1;
+    extra += "1/";
+    numberPartLength -= 2;
+  }
+
   let OOM = Math.floor(Math.log10(Math.max(1, Math.abs(x))));
   let suffixLevel = Math.floor(OOM/3);
   let suffixPart = (suffixLevel <= 10 ? notation1[suffixLevel] : notation2[suffixLevel%10]) + notation3[Math.floor(suffixLevel/10)];
-  let numberPart = (x/10**(suffixLevel*3)).toFixed(Math.max(0, 3-suffixPart.length-OOM%3));
-  if (x < 1000 && Number.isInteger(x)) numberPart = x;
-  return numberPart + suffixPart;
+  numberPartLength -= suffixPart.length;
+  let numberPart = (x/10**(suffixLevel*3)).toFixed(Math.max(0, numberPartLength-OOM%3));
+  if (!focusDecimal && x < 1000 && Number.isInteger(x)) numberPart = x;
+  return extra + numberPart + suffixPart;
 }
