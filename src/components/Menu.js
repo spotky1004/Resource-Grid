5d1c655a0b55fd54d63fb5dc73b97febfded3eb2
Spotky1004 <spotky1004@gmail.com>
2021-10-22 22:10:52 +0900

    chore: Remove unused code

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -200,31 +200,6 @@ const MENU_LIST = [
       const encryptedSavefile = window.prompt("Import Save");
       const toSave = JSON.parse(window.atob(encryptedSavefile));
       store.dispatch(importSave(toSave));
-
-      // from [https://stackoverflow.com/a/65996386/13817471]
-      function copyToClipboard(textToCopy) {
-        // navigator clipboard api needs a secure context (https)
-        if (navigator.clipboard && window.isSecureContext) {
-            // navigator clipboard api method'
-            return navigator.clipboard.writeText(textToCopy);
-        } else {
-            // text area method
-            let textArea = document.createElement("textarea");
-            textArea.value = textToCopy;
-            // make the textarea out of viewport
-            textArea.style.position = "fixed";
-            textArea.style.left = "-999999px";
-            textArea.style.top = "-999999px";
-            document.body.appendChild(textArea);
-            textArea.focus();
-            textArea.select();
-            return new Promise((res, rej) => {
-                // here the magic happens
-                document.execCommand('copy') ? res() : rej();
-                textArea.remove();
-            });
-        }
-      }
     },
   },
   {
